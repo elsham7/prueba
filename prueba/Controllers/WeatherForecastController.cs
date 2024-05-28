fbe6efac8abb9009ca8bacaaa7a5865c26810ae2
salcivar <elsham7@gmail.com>
2024-05-27 19:49:03 -0500

    subida

--- prueba/Controllers/WeatherForecastController.cs
+++ prueba/Controllers/WeatherForecastController.cs
@@ -20,7 +20,7 @@ namespace prueba.Controllers
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
-        {
+        { 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
